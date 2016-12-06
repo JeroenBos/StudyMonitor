5bf7a582762178cc0cfb2e547447b05ceb6e1080
JeroenBos <jer123bos@gmail.com>
2016-12-06 16:39:17 +0100

    Database: Add class TaskTimeSpan

---
+++ StudyMonitor/TaskTimeSpan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyMonitorDatabase
+{
+	public class TaskTimeSpan
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public int TaskId { get; set; }
+		[Required]
+		public DateTime Start { get; set; }
+		[Required]
+		public DateTime End { get; set; }
+	}
+}
