f1e7bdd5675e9e9358b22a132881266c996d7e49
JeroenBos <jer123bos@gmail.com>
2016-12-07 14:31:18 +0100

    MVC: Rewrite default implementation to StudyTask

---
+++ Service/ITasks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service
+{
+	[ServiceContract]
+	public interface IStudyTasks
+	{
+		[OperationContract]
+		void Add(StudyTask task);
+	}
+
+	[DataContract]
+	public class StudyTask
+	{
+		[DataMember]
+		public string Name { get; set; }
+	}
+}
---
+++ Service/StudyTasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+	public class StudyTasks : IStudyTasks
+	{
+		private readonly List<StudyTask> tasks = new List<StudyTask>();
+
+		public void Add(StudyTask task)
+		{
+			tasks.Add(task);
+		}
+	}
+}
