68962c7d35e90c28a944b6f45bf9ecbd5597855d
JeroenBos <jer123bos@gmail.com>
2016-12-07 15:11:39 +0100

    WCF: Make namespaces consistent

--- Service/IStudyTasks.cs
+++ Service/IStudyTasks.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace Service
+namespace StudyMonitor.Service
 {
 	[ServiceContract]
 	public interface IStudyTasks
