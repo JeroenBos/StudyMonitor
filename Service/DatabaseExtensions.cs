52dd226b2602909212d81389ace5bf0e53254c21
JeroenBos <jer123bos@gmail.com>
2016-12-07 15:07:28 +0100

    WCF: Add studyTask.ToEntity()

---
+++ Service/DatabaseExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyMonitor.Service
+{
+	static class DatabaseExtensions
+	{
+		public static StudyTask ToEntity(this Database.StudyTask task)
+		{
+			return new StudyTask()
+			{
+				Name = task.Name
+			};
+		}
+	}
+}
