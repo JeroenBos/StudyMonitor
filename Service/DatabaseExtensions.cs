20068a9e8f67ec71fb75859a7eaadaa331866b75
JeroenBos <jer123bos@gmail.com>
2016-12-08 16:38:02 +0100

    WCF: Add TaskTimeSpan conversion extension methods

--- Service/DatabaseExtensions.cs
+++ Service/DatabaseExtensions.cs
@@ -23,5 +23,31 @@ namespace StudyMonitor.Service
 				Name = task.Name
 			};
 		}
+
+		public static TaskTimeSpanService ToService(this TaskTimeSpan timeSpan, IStudyTasksService service)
+		{
+			if (service == null) throw new ArgumentNullException(nameof(service));
+
+			var task = service.GetTask(timeSpan.TaskId);
+			if (task == null) throw new ArgumentException($"A task with Id {timeSpan.TaskId} does not exist in the database", nameof(timeSpan));
+
+			return new TaskTimeSpanService
+			{
+				Id = timeSpan.Id,
+				Start = timeSpan.Start,
+				End = timeSpan.End,
+				Task = task
+			};
+		}
+		public static TaskTimeSpan ToDBObject(this TaskTimeSpanService timeSpan)
+		{
+			return new TaskTimeSpan
+			{
+				Id = timeSpan.Id,
+				Start = timeSpan.Start,
+				End = timeSpan.End,
+				TaskId = timeSpan.Task.Id
+			};
+		}
 	}
 }
