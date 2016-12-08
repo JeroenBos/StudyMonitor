27436162cae34dadf9f695ca4c1ed707b9236f5f
JeroenBos <jer123bos@gmail.com>
2016-12-08 16:25:16 +0100

    WCF: Add TaskTimeSpanService

---
+++ Service/TaskTimeSpanService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyMonitor.Service
+{
+	[DataContract]
+	public class TaskTimeSpanService
+	{
+		[DataMember]
+		public int Id { get; set; }
+		[DataMember]
+		public DateTime Start { get; set; }
+		[DataMember]
+		public DateTime End { get; set; }
+		[DataMember]
+		public StudyTaskService Task { get; set; }
+	}
+}
