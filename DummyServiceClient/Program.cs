d366e7f8a11255e7c345a2eee1c90397db9b9048
JeroenBos <jer123bos@gmail.com>
2016-12-08 17:01:15 +0100

    WCF Client: Test AddTimeSpanTo(StudyTask)

--- DummyServiceClient/Program.cs
+++ DummyServiceClient/Program.cs
@@ -12,7 +12,11 @@ namespace StudyMonitor.DummyServiceClient
 		static void Main(string[] args)
 		{
 			StudyTasksService tasks = new StudyTasksService();
-			var id = tasks.Add(new StudyTaskService() { Id = 3, Name = "first" });
+			var task = new StudyTaskService() { Id = 3, Name = "first" };
+			var timespan = new TaskTimeSpanService() { Id = 1, End = DateTime.Now, Start = DateTime.Now, Task = task };
+			var id = tasks.Add(task);
+			tasks.AddTimeSpanTo(task, timespan);
+			Console.WriteLine("Added timespan to task");
 			Console.ReadLine();
 		}
 	}
