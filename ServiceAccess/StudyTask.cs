17cab5d7ba3de810d475984a130a26f104aedb9e
JeroenBos <jer123bos@gmail.com>
2016-12-09 12:04:33 +0100

    ServiceAccess: StudyTask attempt

---
+++ ServiceAccess/StudyTask.cs
@@ -0,0 +1,34 @@
+﻿using StudyMonitor.ServiceAccess.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyMonitor.ServiceAccess
+{
+	class StudyTask
+	{
+		private readonly StudyTaskService service;
+		private readonly ObservableCollection<TaskTimeSpan> timeSpans;
+		public string Name { get; }
+		public ReadOnlyObservableCollection<TaskTimeSpan> TimeSpans { get; }
+
+		public StudyTask(StudyTaskService service)
+		{
+			this.service = service ?? throw new ArgumentNullException(nameof(service));
+			Name = service.Name;
+			timeSpans = new ObservableCollection<TaskTimeSpan>();
+			TimeSpans = new ReadOnlyObservableCollection<TaskTimeSpan>(this.timeSpans);
+		}
+	}
+	class TaskTimeSpan
+	{
+		private readonly TaskTimeSpanService service;
+		public TaskTimeSpan(TaskTimeSpanService service)
+		{
+			this.service = service ?? throw new ArgumentNullException(nameof(service));
+		}
+	}
+}
