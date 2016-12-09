0771aef9ed86a90be9fc04afb9f59e62db6a401d
JeroenBos <jer123bos@gmail.com>
2016-12-09 12:07:54 +0100

    ServiceAccess: Make StudyTask.TimeSpans modifiable

--- ServiceAccess/StudyTask.cs
+++ ServiceAccess/StudyTask.cs
@@ -11,16 +11,14 @@ namespace StudyMonitor.ServiceAccess
 	class StudyTask
 	{
 		private readonly StudyTaskService service;
-		private readonly ObservableCollection<TaskTimeSpan> timeSpans;
 		public string Name { get; }
-		public ReadOnlyObservableCollection<TaskTimeSpan> TimeSpans { get; }
+		public ObservableCollection<TaskTimeSpan> TimeSpans { get; }
 
 		public StudyTask(StudyTaskService service)
 		{
 			this.service = service ?? throw new ArgumentNullException(nameof(service));
 			Name = service.Name;
-			timeSpans = new ObservableCollection<TaskTimeSpan>();
-			TimeSpans = new ReadOnlyObservableCollection<TaskTimeSpan>(this.timeSpans);
+			TimeSpans = new ObservableCollection<TaskTimeSpan>();
 		}
 	}
 	class TaskTimeSpan
