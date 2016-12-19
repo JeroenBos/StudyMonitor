1cb43a48b4b3c7fbdec9f3afd2150471a1ab1b96
Erik <eptorck@hotmail.com>
2016-12-19 14:19:10 +0100

    fixed 2 errors, only sql error remaining (hopefully)

---
+++ ServiceAccesTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyMonitor.ServiceAccess.ServiceReference;
+
+
+namespace ServiceAccesTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        public static void Main(string[] args)
+        {
+            new UnitTests().test();
+            Console.ReadLine();
+        }
+        private readonly StudyTaskService service = new StudyTaskService();
+        private readonly StudyTasksServiceClient client = new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
+
+        [TestCleanup]
+        public void calledEverytimeAfterATest()
+        {
+            //client.ClearAll();
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            client.Add(new StudyTaskService());
+        }
+    }
+
+
+}
