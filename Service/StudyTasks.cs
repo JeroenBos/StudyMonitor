bcdcfde499a5cdf05d8b567d0bf30b15f3966a38
JeroenBos <jer123bos@gmail.com>
2016-12-20 14:40:37 +0100

    WCF: Implement GetOpenTimeSpanIdFor

--- Service/StudyTasks.cs
+++ Service/StudyTasks.cs
@@ -1,6 +1,7 @@
 ﻿using StudyMonitor.Database;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -93,6 +94,30 @@ namespace StudyMonitor.Service
 			using (var context = new StudyTasksContext())
 			{
 				return context.Tasks.ToList().Select(x => x.ToService());
+			}
+		}
+
+		/// <summary> Gets the ID of the open time span (i.e. with <see cref="TaskTimeSpan.End"/> == null) pertaining to the specified task id; 
+		/// or 0 in case there is no such open time span. </summary>
+		public int GetOpenTimeSpanIdFor(int taskId)
+		{
+			using (var context = new StudyTasksContext())
+			{
+				var openTimeSpansDB = context.TimeSpans
+											 .Where(timeSpanDB => timeSpanDB.TaskId == taskId)
+											 .Where(timeSpanDB => timeSpanDB.End == null)
+											 .ToList();
+
+				Debug.Assert(openTimeSpansDB.Count <= 1, "There should at most one open timespan per task");
+
+				if(openTimeSpansDB.Count == 1)
+				{
+					return openTimeSpansDB[0].Id;
+				}
+				else
+				{
+					return 0;
+				}
 			}
 		}
 	}
--- ServiceAccess/Service Capabilities/ServiceReference/Reference.cs
+++ ServiceAccess/Service Capabilities/ServiceReference/Reference.cs
@@ -206,6 +206,12 @@ namespace StudyMonitor.ServiceAccess.ServiceReference {
         
         [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IStudyTasksService/GetAllTasks", ReplyAction="http://tempuri.org/IStudyTasksService/GetAllTasksResponse")]
         System.Threading.Tasks.Task<StudyMonitor.ServiceAccess.ServiceReference.StudyTaskService[]> GetAllTasksAsync();
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IStudyTasksService/GetOpenTimeSpanIdFor", ReplyAction="http://tempuri.org/IStudyTasksService/GetOpenTimeSpanIdForResponse")]
+        int GetOpenTimeSpanIdFor(int taskId);
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IStudyTasksService/GetOpenTimeSpanIdFor", ReplyAction="http://tempuri.org/IStudyTasksService/GetOpenTimeSpanIdForResponse")]
+        System.Threading.Tasks.Task<int> GetOpenTimeSpanIdForAsync(int taskId);
     }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
@@ -281,6 +287,14 @@ namespace StudyMonitor.ServiceAccess.ServiceReference {
         
         public System.Threading.Tasks.Task<StudyMonitor.ServiceAccess.ServiceReference.StudyTaskService[]> GetAllTasksAsync() {
             return base.Channel.GetAllTasksAsync();
+        }
+        
+        public int GetOpenTimeSpanIdFor(int taskId) {
+            return base.Channel.GetOpenTimeSpanIdFor(taskId);
+        }
+        
+        public System.Threading.Tasks.Task<int> GetOpenTimeSpanIdForAsync(int taskId) {
+            return base.Channel.GetOpenTimeSpanIdForAsync(taskId);
         }
     }
 }
