cfaa598e1b6c878b6265f254a66fad36a4039f6c
JeroenBos <jer123bos@gmail.com>
2016-12-20 15:02:18 +0100

    Initialize ServiceAccessUnitTests

---
+++ ServiceAccess.Tests/ServiceAccessUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyMonitor.ServiceAccess.Tests
+{
+	[TestClass]
+	public class ServiceAccessUnitTests
+	{
+		[TestMethod]
+		public void TestMethod()
+		{
+
+		}
+	}
+}
