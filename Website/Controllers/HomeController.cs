a1c4545b6e8690872ce1a94d1458d2f5f1aa4763
JeroenBos <jer123bos@gmail.com>
2016-12-20 11:05:26 +0100

    Make sure first task exists

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -3,30 +3,30 @@ using StudyMonitor.ServiceAccess.ServiceReference;
 
 namespace Website.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            StudyTaskService service = new StudyTaskService();
-            StudyTasksServiceClient client = new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
-            var task = client.GetTask(1);
-            ViewBag.Id = task.Id;
-            ViewBag.Name = task.Name;
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			StudyTaskService service = new StudyTaskService();
+			StudyTasksServiceClient client = new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
+			var task = client.GetTask(client.Add(new StudyTaskService() { Name = "Erik" }));
+			ViewBag.Id = task.Id;
+			ViewBag.Name = task.Name;
+			return View();
+		}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-    }
+			return View();
+		}
+	}
 }
