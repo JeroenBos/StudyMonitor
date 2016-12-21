fe5327a27a91443b006f88cd5c98133ad88eccb4
JeroenBos <jer123bos@gmail.com>
2016-12-21 10:28:01 +0100

    Refactor client construction in website

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -8,19 +8,19 @@ using Website.Models;
 
 namespace Website.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            var client = new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
-            client.Add(new StudyTaskService() { Name = "TestCase" });
-            var studyTasksModel = new StudyTasksModel
-            {
-                StudyTaskModels = client.GetAllTasks().Select(
-                    e => new StudyTaskModel() { Id = e.Id, Name = e.Name }).ToList()
-            };
-            return View(studyTasksModel);
-        }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var client = CreateTasksClient();
+			client.Add(new StudyTaskService() { Name = "TestCase" });
+			var studyTasksModel = new StudyTasksModel
+			{
+				StudyTaskModels = client.GetAllTasks().Select(
+					e => new StudyTaskModel() { Id = e.Id, Name = e.Name }).ToList()
+			};
+			return View(studyTasksModel);
+		}
 
 		public ActionResult About()
 		{
@@ -33,42 +33,48 @@ namespace Website.Controllers
 		{
 			ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
+			return View();
+		}
 
-        
+
 		public ActionResult Select(object data)
 		{
-            string taskId = ((string[])data)[0];
-            int id;
-            bool validData = int.TryParse(taskId, out id);
-            if (validData)
-            {
-                var startingTaskItemTimeSpan = new TaskTimeSpanService()
-                {
-                    End = null,
-                    TaskId = id,
-                    Start = DateTime.Now
-                };
+			string taskId = ((string[])data)[0];
+			int id;
+			bool validData = int.TryParse(taskId, out id);
+			if (validData)
+			{
+				var startingTaskItemTimeSpan = new TaskTimeSpanService()
+				{
+					End = null,
+					TaskId = id,
+					Start = DateTime.Now
+				};
 
-                StudyTasksServiceClient client = new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
-                client.AddTimeSpanTo(id, startingTaskItemTimeSpan);
-            }
+				StudyTasksServiceClient client = CreateTasksClient();
+				client.AddTimeSpanTo(id, startingTaskItemTimeSpan);
+			}
 
-            return View("Index");
-        }
+			return View("Index");
+		}
 
-        public ActionResult Add(object data)
-        {
-            string taskName = ((string[]) data)[0];
-            // Check the string for a valid task name
-            if (true)
-            {
-                StudyTasksServiceClient client = new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
-                client.Add(new StudyTaskService() {Name= taskName});
-            }
+		public ActionResult Add(object data)
+		{
+			string taskName = ((string[])data)[0];
+			// Check the string for a valid task name
+			if (true)
+			{
+				StudyTasksServiceClient client = CreateTasksClient();
+				client.Add(new StudyTaskService() { Name = taskName });
+			}
 
-            return View("Index");
-        }
+			return View("Index");
+		}
+
+		/// <summary> Encapsulates the construction of a <see cref="StudyTasksServiceClient"/>. </summary>
+		private StudyTasksServiceClient CreateTasksClient()
+		{
+			return new StudyTasksServiceClient("BasicHttpBinding_IStudyTasksService");
+		}
 	}
 }
