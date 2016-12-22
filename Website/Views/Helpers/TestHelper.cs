b5baa7a599a677cd4a48649ecef18d5527474029
JeroenBos <jer123bos@gmail.com>
2016-12-22 15:51:15 +0100

    Html helper first attempt

---
+++ Website/Views/Helpers/TestHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Views.Helpers
+{
+	public class TestHelper
+	{
+		public static HtmlString Note(string content)
+		{
+			var result = $@"<div class='note' style='border: 1px solid black; width: 90%; padding: 5px; margin-left: 15px;'>	
+								<p>	<strong> Note </strong> 
+										&nbsp; &nbsp; {content}
+								</p>
+							</div> ";
+
+			return new HtmlString(result);
+		}
+	}
+}
